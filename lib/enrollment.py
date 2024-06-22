1ebe80f77a5fe9eedca7aeb447c1569bbd98e23b
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-06-22 09:40:13 +0300

    final

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -43,5 +43,5 @@ class Enrollment:
         else:
             raise TypeError("Invalid types for student and/or course")
 
-    def get_enrollment_date(self):
+    def  get_enrollment_date(self):
         return self._enrollment_date
